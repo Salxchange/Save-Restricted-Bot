24799dfc216086ba8462b3c93d0a5dd51f4a7f68
Salxchange <138751618+Salxchange@users.noreply.github.com>
2023-12-04 10:06:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ def progress(current, total, message, type):
 # start command
 @bot.on_message(filters.command(["start"]))
 def send_start(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
-	bot.send_message(message.chat.id, f"__👋 Hi **{message.from_user.mention}**, I am Save Restricted Bot, I can send you restricted content by it's post link__,
+	bot.send_message(message.chat.id, f"__👋 Hi **{message.from_user.mention}**, I am Save Restricted Bot, I can send you restricted content by it's post link,
 	        reply_markup=InlineKeyboardMarkup(
             [
                 [InlineKeyboardButton("🧑‍💻 𝖣𝖾𝗏𝖾𝗅𝗈𝗉𝖾𝗋", url="https://t.me/Snowball_Official")],
